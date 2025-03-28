fe3b40bd7ad37a6061e0e8c57b32c8f3ea9da032
Massimo223 <s308028@studenti.polito.it>
2025-03-28 10:27:51 +0100

    aggiorna

---
+++ Exercise/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define STACK_SIZE 8
+
+template<typename T>
+class complex_number {
+	T parte_reale;
+    T parte_immaginaria;
+	public:
+	
+}
+stack::stack() {
+    top = 0;
+}
+int main()
+{
+
+}
